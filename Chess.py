bddf4b56f1afa15a4077ec703be658541d23adce
Rajinish0 <66392735+Rajinish0@users.noreply.github.com>
2021-02-24 00:29:45 +0500

    Update Chess.py

--- Chess.py
+++ Chess.py
@@ -8,8 +8,8 @@ blocks = 8
 blockW = w//blocks
 blockH = h//blocks
 
-def CheckEvent():
-	global run,pieceinHand,piece,curValidMoves,lastLegalPos,curfunc,funcs,KINGSPOS,InCheck,CheckMate
+def CheckEvent(functionality_disabled=False):
+	global run,pieceinHand,piece,curValidMoves,lastLegalPos,curfunc,funcs,KINGSPOS,InCheck,CheckMate,EnPassantMoves_
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			pygame.QUIT
@@ -25,7 +25,6 @@ def CheckEvent():
 					pieceinHand = True
 					curValidMoves = MOVEFUNCS[piece.lower()](i,j,funcs[curfunc])
 					board[i][j] = ''
-					# curValidMoves = FilterMoves(piece,funcs[curfunc],curValidMoves);
 
 
 			if event.type == pygame.MOUSEBUTTONUP:
@@ -46,10 +45,13 @@ def CheckEvent():
 					if board[i][j].lower() == 'k':															### UPDAATE KING's Position
 						KINGSPOS[funcs[not curfunc]] = (i,j) if not (i,j) == lastLegalPos else KINGSPOS[funcs[not curfunc]]
 
-					lastLegalPos = None					
+					CheckPawnHistory(i,j,curfunc)
+					CheckEnPassantKill(i,j,curfunc)
 
-					CheckForPawnPromotion(i,j,funcs[not curfunc]);
+					EnPassantMoves_ = []
+					lastLegalPos = None
 
+					CheckForPawnPromotion(i,j,funcs[not curfunc]);								
 					CheckForCheckMate();
 
 
@@ -63,6 +65,26 @@ def GetBlockFromMouse():
 	return j,i
 
 
+'''
+So if a pawn is at starting position and it moves 2 steps, i record that pawn in a dictionary
+then when the other pawn moves and the pawn to its left or right has moved 2 steps it has the right to kill it.
+the move is added to EnPassantMoves_. The rules for enpassant being applicable only once for every pawn was quite simple to implement
+the record in dictionary gets changed to 0 every time a pawn moves from its starting position and EnPassantMoves_ gets emptied every time a move is played.
+'''
+
+def CheckPawnHistory(i,j,curfunc):
+	global lastLegalPos, funcs
+	if board[i][j].lower() == 'p' and lastLegalPos != (i,j) and (lastLegalPos[0] == 1 or lastLegalPos[0] == 6):
+		ResetPawnHistory()
+		UpdatePawnHistory(lastLegalPos,(i,j),funcs[not curfunc]);
+
+def CheckEnPassantKill(i,j,curfunc):
+	global EnPassantMoves_,funcs,lastLegalPos
+	if board[i][j].lower() == 'p' and lastLegalPos != (i,j) and ((i,j) in EnPassantMoves_):
+		print('KILLING',i,j)
+		DoTheDeed(i,j,funcs[curfunc]);
+
+
 def CheckForPawnPromotion(i,j,func):
 	global imgs,board,CheckMate
 	if board[i][j].lower() == 'p' and (i==0 or i == 7):
@@ -70,7 +92,7 @@ def CheckForPawnPromotion(i,j,func):
 		screen.fill((0,0,0))
 		drawRects()
 		drawPieces(board,imgs)
-		DrawCheck((i,j),(255,255,0))					## BAD FUNCTION NAMING; THIS JUST DRAWS A RECT AT A GIVEN POSITION
+		DrawCheck((i,j),(255,255,0))			## BAD FUNCTION NAMING; THIS JUST DRAWS A RECT AT A GIVEN POSITION
 		pygame.display.update()
 		CheckMate= True;						## JUST TO DISABLE ANY MOUSE CLICKS DURING PAWN PROMOTION PHASE.
 		PawnPromotion(i,j,func);
@@ -111,7 +133,6 @@ def PawnPromotion(i,j,func):
 			board[i][j] = newFunc('p')
 			break
 		CheckEvent()
-
 
 def movePiece(d,piece):
 	posx,posy = pygame.mouse.get_pos()
@@ -181,8 +202,32 @@ def PawnMoves(i,j,func,lookingFor=[]):
 	validMoves = validMoves + potentialMove1 if enemyOnRDiag else validMoves
 	validMoves = validMoves + potentialMove2 if enemyOnLDiag else validMoves
 
+	validMoves = validMoves + CheckForEnPassant(i,j,func) if lookingFor == [] else validMoves;
+
 	return validMoves+MOVE if lookingFor == [] else (enemyOnRDiag or enemyOnLDiag)
 
+
+def CheckForEnPassant(i,j,func):
+	global PawnHistory, EnPassantMoves_
+	LookUpFunc = str.islower if func == str.isupper else str.isupper
+	NewFunc = str.lower if func == str.isupper else str.upper
+	sign = 1 if func == str.islower else -1
+	EnPassantMoves = []
+
+	lookingFor = [NewFunc('p')]
+	move, enemyOnLeft = MOVECHECKER(i,j,func,0,-1,orig=1,depth=2,valMoves =[],lookingFor=lookingFor)
+	move2, enemyOnRight = MOVECHECKER(i,j,func,0,1,orig=1,depth=2,valMoves=[],lookingFor=lookingFor)
+
+	EnPassantMoves = EnPassantMoves + [(i+1*sign,j-1)] if (enemyOnLeft and PawnHistory[LookUpFunc][j-1] == 2) else EnPassantMoves
+	EnPassantMoves = EnPassantMoves + [(i+1*sign,j+1)] if (enemyOnRight and PawnHistory[LookUpFunc][j+1] == 2) else EnPassantMoves
+	EnPassantMoves_ += EnPassantMoves
+
+	return EnPassantMoves
+
+def DoTheDeed(i,j,func):				## EN PASSANT BRUTAL KILL.
+	global board 
+	sign = 1 if func == str.islower else -1
+	board[i+sign][j] = ''
 
 
 def KingMoves(i,j,func,lookingFor=[]):
@@ -215,22 +260,30 @@ def KnightMoves(i,j,func,lookingFor=[]):
 	return validMoves;
 
 
+'''
+MOVECHECKER - 
+I obv went crazy with if else commands
+Here's the gist of the code
+gets a moveI and moveJ, which come from different pieces
+Moves recursively in that moveI, moveJ until the conditions above are satisfied		
+Also works as a look up function; which I used for KingInCheck, really couldn't go through
+the trouble of writing another function for that. Takes care of not adding a position if that would
+Make the king in check.'''
 def MOVECHECKER(i,j,func,moveI,moveJ,valMoves=[],orig=False,depth=50,lookingFor=[],found=False,piece=None):
-	global board
 	##FUNC btw can be str.isupper, which would be white, or str.islower for black (just built up from fenstrings.)
 	##SO if func(board[i][j]); meaning if the piece which im checking the moves for is white and the board's position which im currently at is white too
 	##Then i want this to terminate obv.
+	global board
 	if (i >= blocks or j>=blocks or i < 0 or j< 0 or func(board[i][j]) or depth <= 0 or found) and (not orig):
 		return valMoves if lookingFor == [] else (valMoves,found)
 
-	piece = copy.copy(board[i][j]) if (orig and (lookingFor == [] or board[i][j].lower() == 'p')) else piece 	##UH used lookingFor for pawn, had to use another condition.
-	board[i][j] = '' if (piece is not None and orig) else board[i][j]							## went crazy with if else commands
-													##Here's the gist of the code
-	if not orig and piece is not None:								##gets a moveI and moveJ, which come from different pieces
-		origPiece = copy.copy(board[i][j])				#Moves recursively in that moveI, moveJ until the conditions above are satisfied		
-		board[i][j] = piece						##Also works as a look up function; which I used for KingInCheck, really couldn't go through
-		if piece.lower() =='k':						#the trouble of writing another function for that. Takes care of not adding a position if that would
-			origKingPos = copy.copy(KINGSPOS[func])		#Make the king in check.
+	piece = copy.copy(board[i][j]) if (orig and (lookingFor == [] or board[i][j].lower() == 'p')) else piece #UH used lookingFor for pawn, had to use another condition.
+
+	if not orig and piece is not None:
+		origPiece = copy.copy(board[i][j])	
+		board[i][j] = piece
+		if piece.lower() =='k':
+			origKingPos = copy.copy(KINGSPOS[func])
 			KINGSPOS[func] = (i,j)	
 		if not KingInCheck(*KINGSPOS[func],func):
 			valMoves.append((i,j))
@@ -246,17 +299,30 @@ def MOVECHECKER(i,j,func,moveI,moveJ,valMoves=[],orig=False,depth=50,lookingFor=
 	return res
 
 
+	
 def KingInCheck(i,j,func):
-	newFunc = str.lower if func == str.isupper else str.upper			#TO SEE IF KING IS IN CHECK, MOVE EVERY piece's moves from KING'S current position
-	pieces = ['q','b','r','p','k','n']						#And see if the piece whose move you are moving is on one of those blocks.
-	pieces = [newFunc(each) for each in pieces]
-
-	c1 = QueenMoves(i,j,func,lookingFor = pieces[:3])
+	newFunc = str.lower if func == str.isupper else str.upper
+	pieces = ['q','b','r','p','k','n']
+	pieces = [newFunc(each) for each in pieces]										#TO SEE IF KING IS IN CHECK, MOVE EVERY piece's moves from KING'S current position
+	c1 = QueenMoves(i,j,func,lookingFor = pieces[:3])								#And see if the piece whose move you are moving is on one of those blocks.
 	c2 = PawnMoves(i,j,func,lookingFor=pieces[3])
 	c3 = KingMoves(i,j,func,lookingFor=pieces[4])
 	c4 = KnightMoves(i,j,func,lookingFor=pieces[-1])
 
 	return (c1 or c2 or c3 or c4)
+
+
+def ResetPawnHistory():
+	global PawnHistory
+	for each in list(PawnHistory.keys()):
+		for j in list(PawnHistory[each].keys()):
+			PawnHistory[each][j] = 0
+
+def UpdatePawnHistory(lastLegalPos,newPos,func):
+	global PawnHistory
+	oldI, oldJ = lastLegalPos;
+	i, j = newPos;
+	PawnHistory[func][j] = abs(oldI - i)
 
 
 def parseFen(board,fen):
@@ -288,7 +354,9 @@ def drawCurValidMoves(curValidMoves):
 	for each in curValidMoves:
 		x,y = blockW*each[1],blockH*each[0]
 		r = Rect(x,y,blockW,blockH)
-		pygame.draw.rect(screen,(0, 0, 51),r,5)
+		#c = (0, 0, 51)
+		pygame.draw.rect(screen,(51,51,81),r)
+		pygame.draw.rect(screen,(0,0,51),r,5)
 
 
 def drawPieces(board,imgs):
@@ -301,23 +369,32 @@ def drawPieces(board,imgs):
 			x,y = blockW*j,blockH *i
 			screen.blit(img,(x+10,y-10))
 
-def DrawCheck(pos):
+def DrawCheck(pos,col=None):
 	global CheckMate
 	i,j = pos
 	x,y = blockW*j,blockH *i
 	r = Rect(x,y,blockW,blockH)
 	c = (155,0,0) if not CheckMate else (155,0,155)
+	c = c if col is None else col
 	pygame.draw.rect(screen,c,r,5)
 
+
+'''
+IF KING Can't move anywhere, then check for all moves
+of all pieces of same color, if their possible moves are more then one
+(1 move would be their own position) that means checkmate could be avoided
+MOVECHECKER doesn't add the position in valid moves, which would cause the king
+to be in check.
+'''
 
 ## THE WORST ALGORITHM FOR CHECKING FOR CHECK MATE
 def LousyCheckMateAlgo(func):
 	piecesToCheck = ['q','r','n','b','p'];
-	newFunc = str.upper if func == str.isupper else str.lower				#IF KING Can't move anywhere, then check for all moves
-	piecesToCheck = [newFunc(each) for each in piecesToCheck];				#of all pieces of same color, if their possible moves are more then one
-	for i in range(len(board)):								#(1 move would be their own position) that means checkmate could be avoided
-		for j in range(len(board[0])):							#MOVECHECKER doesn't add the position in valid moves, which would cause the king
-			if board[i][j] in piecesToCheck:					#to be in check.
+	newFunc = str.upper if func == str.isupper else str.lower
+	piecesToCheck = [newFunc(each) for each in piecesToCheck];
+	for i in range(len(board)):
+		for j in range(len(board[0])):
+			if board[i][j] in piecesToCheck:
 				possibleMoves = MOVEFUNCS[board[i][j].lower()](i,j,func)
 				if len(possibleMoves) > 1:
 					return False
@@ -335,7 +412,10 @@ funcs = {0:str.islower,1:str.isupper}
 KINGSPOS = {str.islower:(0,4),str.isupper:(7,4)}                  ## CHANGE KINGS' POSITIONS FOR DEFAULT STARTING FEN; or write a function that does that for you.
 InCheck = None
 curfunc = 1
+PawnHistory = {str.islower:{i:0 for i in range(8)},
+			   str.isupper:{i:0 for i in range(8)},}			##FOR MEASURING CHANGE IN POSITION
 fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR'
+EnPassantMoves_ = []
 #fen = '4k3/1R6/2QQ5/8/8/8/8/4K3'
 imgs= loadPicsDict()
 board = GetBoard()
@@ -344,11 +424,13 @@ parseFen(board,fen)
 while run:
 	screen.fill((0,0,0))
 	drawRects()
-	drawPieces(board,imgs)
-	keys,run,piece = CheckEvent()
 	if pieceinHand:
 		drawCurValidMoves(curValidMoves);
+		drawPieces(board,imgs)
 		movePiece(imgs,piece)
+	else:
+		drawPieces(board,imgs)
+	keys,run,piece = CheckEvent()
 	if InCheck is not None:
 		DrawCheck(InCheck);
 	pygame.display.update()
